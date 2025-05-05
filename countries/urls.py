da9f76789f2b3e6c00b449fa6d357da377a5bc79
Maruf Monower <marufmonower@gmail.com>
2025-05-05 18:24:28 +0600

    Adding templates

--- countries/urls.py
+++ countries/urls.py
@@ -1,10 +1,13 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path,include
 from .views import CountryViewSet
+from .import views
 
 router = DefaultRouter()
 router.register(r'countries', CountryViewSet,basename='country')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
+    path('countries/',views.country_list,name = 'country_list'),
+    path('countries/<int:pk>/details/',views.country_details,name = 'country_details'),
 ]
--- countries/views.py
+++ countries/views.py
@@ -19,10 +19,33 @@ class CountryViewSet(viewsets.ModelViewSet):
         same_region_countries = Country.objects.filter(
             region=country.region).exclude(pk=country.pk)
         serializer = self.get_serializer(same_region_countries, many=True)
-        return Response(serializer.data) 
+        return Response(serializer.data)
 
     @action(detail=False, methods=['get'], url_path='by-language/(?P<lang>\w+)')
     def by_language(self, request, lang=None):
         countries = Country.objects.filter(languages__icontains=lang)
         serializer = self.get_serializer(countries, many=True)
         return Response(serializer.data)
+
+
+def country_list(request):
+    search_query = request.GET.get('search', '')
+    countries = Country.objects.all()
+
+    if search_query:
+        countries = countries.filter(name__icontains=search_query)
+
+    return render(request, 'countries/country_list.html', {'countries': countries})
+
+
+def country_details(request, pk):
+
+    country = get_object_or_404(Country, pk=pk)
+    same_region = Country.objects.filter(
+        region=country.region).exclude(pk=country.pk)
+    languages = country.languages.split(',')#.all()
+    return render(request, 'countries/country_details.html', {
+        'country': country,
+        'same_region': same_region,
+        'languages': languages,
+    })
